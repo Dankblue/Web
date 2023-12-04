3a7b426bf97920526ebc980461f76505b463d16b
ivan Petrovic <ivanpetrovic2005@gmail.com>
2023-12-04 10:45:40 +0100

    sredjivanje tekst boksa

--- Skripta.js
+++ Skripta.js
@@ -6,3 +6,19 @@ function toggleMenu() {
       navLinks.style.display = "flex";
     }
   }
+  function filterReviews()
+  {
+      const searchInput = document.getElementById('search').value.toLowerCase();
+      const reviewsContainer = document.getElementById('reviews-container');
+
+      reviewsData.forEach((review, index) => {
+          const reviewElement = reviewsContainer.children[index];
+          if (review.text.toLowerCase().includes(searchInput))
+          {
+              reviewElement.style.display = 'block';
+          } else
+          {
+              reviewElement.style.display = 'none';
+          }
+      });
+  }
